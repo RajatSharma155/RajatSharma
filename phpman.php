d342c3b335bdac8ce46bdca7aef425fa83cdaa2e
Rajat Sharma <evening23@tamu.edu>
2024-10-09 17:37:46 -0500

    First change in 2024

--- phpman.php
+++ phpman.php
@@ -1,18 +1,28 @@
 <?php
-if (isset($POST['submit']))
-{
-    $name = $_POST['name'];
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
-	$email_form = $POST['mail'];
-	
-	$to = "aspirarajat155@gmail"
-	$headers = "From:" .$email_from "\r\n";
-    $txt = "You have received an email from" .$name.".\n\n".$message;
-	mail($to, $subject, $txt, $headers);
-	header("Location: index.php?mailsend");
+// Check if form is submitted
+if (isset($_POST['submit'])) {
+    // Retrieve form data
+    $name = $_POST['name'];          // Name from the form
+    $subject = $_POST['subject'];    // Subject from the form
+    $message = $_POST['message'];    // Message from the form
+    $email_from = $_POST['email'];   // Email address of the sender
+    
+    // Define recipient email address
+    $to = "aspirarajat155@gmail.com";  // The recipient email address
+    
+    // Set email headers
+    $headers = "From: " . $email_from . "\r\n";
+    
+    // Email body content
+    $txt = "You have received an email from " . $name . ".\n\n" . $message;
+    
+    // Send the email
+    if (mail($to, $subject, $txt, $headers)) {
+        // Redirect to a confirmation page after successful email sending
+        header("Location: index.php?mailsend=success");
+        exit();
+    } else {
+        echo "Mail sending failed!";
+    }
 }
 ?>
-	
-	
-	
